5e8f55ac56879ee3e6973f13b5082168152a287a
amberlin10706 <yyyy2248@hotmail.com>
2022-04-24 22:56:02 +0800

    add firebase

---
+++ src/firebase/database.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from "../../config/firebaseConfig.js";
+import { getFirestore, getDoc, doc, setDoc } from 'firebase/firestore';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function saveData(dto) {
+    const docRef = doc(db, dto.collection, dto.doc);
+    try {
+        const res = await setDoc(docRef, dto.data)
+        console.log("Save Success: ", res);
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
+}
+
+export async function getData(dto) {
+    const docRef = doc(db, dto.collection, dto.doc);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return docSnap.data()
+    }
+}
